7de3f6b32d81a0351d0d6afc6c238ab521936645
Mike Stegeman <mrstegeman@gmail.com>
2018-04-03 11:18:35 -0600

    Packaging cleanups.

--- package.sh
+++ package.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-rm -rf node_modules
-npm install --production
 rm -f SHA256SUMS
 sha256sum package.json *.js LICENSE espThing.ino phpThings.tgz > SHA256SUMS
-find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
 TARFILE=$(npm pack)
-tar xzf ${TARFILE}
-cp -r node_modules ./package
-tar czf ${TARFILE} package
 rm -rf package
 echo "Created ${TARFILE}"
